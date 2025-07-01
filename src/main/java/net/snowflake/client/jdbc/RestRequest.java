a96b23dcbd94dd8f61d2f50bca7a7cfb70659b0b
Ashrith Bandla <ashrith.bandla@snowflake.com>
2025-07-01 21:41:38 +0000

    back to default behavior

--- src/main/java/net/snowflake/client/jdbc/RestRequest.java
+++ src/main/java/net/snowflake/client/jdbc/RestRequest.java
@@ -316,21 +316,21 @@ public class RestRequest {
       int retryCount,
       long retryTimeoutInMilliseconds,
       long elapsedMilliForTransientIssues) {
-    long backoffInMilli=200;
-    // if (isLoginRequest) {
-    //   long jitteredBackoffInMilli =
-    //       decorrelatedJitterBackoff.getJitterForLogin(previousBackoffInMilli);
-    //   backoffInMilli =
-    //       (long)
-    //           decorrelatedJitterBackoff.chooseRandom(
-    //               jitteredBackoffInMilli + previousBackoffInMilli,
-    //               Math.pow(2, retryCount) + jitteredBackoffInMilli);
-    // } else {
+    long backoffInMilli;
+    if (isLoginRequest) {
+      long jitteredBackoffInMilli =
+          decorrelatedJitterBackoff.getJitterForLogin(previousBackoffInMilli);
+      backoffInMilli =
+          (long)
+              decorrelatedJitterBackoff.chooseRandom(
+                  jitteredBackoffInMilli + previousBackoffInMilli,
+                  Math.pow(2, retryCount) + jitteredBackoffInMilli);
+    } else {
 
-    //   backoffInMilli = decorrelatedJitterBackoff.nextSleepTime(previousBackoffInMilli);
-    // }
+      backoffInMilli = decorrelatedJitterBackoff.nextSleepTime(previousBackoffInMilli);
+    }
 
-    // backoffInMilli = Math.min(maxBackoffInMilli, Math.max(previousBackoffInMilli, backoffInMilli));
+    backoffInMilli = Math.min(maxBackoffInMilli, Math.max(previousBackoffInMilli, backoffInMilli));
 
     if (retryTimeoutInMilliseconds > 0
         && (elapsedMilliForTransientIssues + backoffInMilli) > retryTimeoutInMilliseconds) {
