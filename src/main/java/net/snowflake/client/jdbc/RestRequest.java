5e869f29180ff783412eba858193c4f165c0fc53
Ashrith Bandla <ashrith.bandla@snowflake.com>
2025-07-01 21:41:38 +0000

    back to default behavior

--- src/main/java/net/snowflake/client/jdbc/RestRequest.java
+++ src/main/java/net/snowflake/client/jdbc/RestRequest.java
@@ -316,19 +316,19 @@ public class RestRequest {
       int retryCount,
       long retryTimeoutInMilliseconds,
       long elapsedMilliForTransientIssues) {
-    long backoffInMilli=500;
-    // if (isLoginRequest) {
-    //   long jitteredBackoffInMilli =
-    //       decorrelatedJitterBackoff.getJitterForLogin(previousBackoffInMilli);
-    //   backoffInMilli =
-    //       (long)
-    //           decorrelatedJitterBackoff.chooseRandom(
-    //               jitteredBackoffInMilli + previousBackoffInMilli,
-    //               Math.pow(2, retryCount) + jitteredBackoffInMilli);
-    // } else {
+    long backoffInMilli;
+    if (isLoginRequest) {
+      long jitteredBackoffInMilli =
+          decorrelatedJitterBackoff.getJitterForLogin(previousBackoffInMilli);
+      backoffInMilli =
+          (long)
+              decorrelatedJitterBackoff.chooseRandom(
+                  jitteredBackoffInMilli + previousBackoffInMilli,
+                  Math.pow(2, retryCount) + jitteredBackoffInMilli);
+    } else {
 
-    //   backoffInMilli = decorrelatedJitterBackoff.nextSleepTime(previousBackoffInMilli);
-    // }
+      backoffInMilli = decorrelatedJitterBackoff.nextSleepTime(previousBackoffInMilli);
+    }
 
     backoffInMilli = Math.min(maxBackoffInMilli, Math.max(previousBackoffInMilli, backoffInMilli));
 
