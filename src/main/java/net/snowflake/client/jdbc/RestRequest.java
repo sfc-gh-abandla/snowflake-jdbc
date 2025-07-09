cab40417d10f3cd42dc358cc085e1d792860a97b
Ashrith Bandla <ashrith.bandla@snowflake.com>
2025-07-09 23:38:08 +0000

    now fully back to default

--- src/main/java/net/snowflake/client/jdbc/RestRequest.java
+++ src/main/java/net/snowflake/client/jdbc/RestRequest.java
@@ -331,24 +331,24 @@ public class RestRequest {
       backoffInMilli = decorrelatedJitterBackoff.nextSleepTime(previousBackoffInMilli);
     }
 
-    // backoffInMilli = Math.min(maxBackoffInMilli, Math.max(previousBackoffInMilli, backoffInMilli));
+    backoffInMilli = Math.min(maxBackoffInMilli, Math.max(previousBackoffInMilli, backoffInMilli));
 
-    // if (retryTimeoutInMilliseconds > 0
-    //     && (elapsedMilliForTransientIssues + backoffInMilli) > retryTimeoutInMilliseconds) {
-    //   // If the timeout will be reached before the next backoff, just use the remaining
-    //   // time (but cannot be negative) - this is the only place when backoff is not in range
-    //   // min-max.
-    //   backoffInMilli =
-    //       Math.max(
-    //           0,
-    //           Math.min(
-    //               backoffInMilli, retryTimeoutInMilliseconds - elapsedMilliForTransientIssues));
-    //   logger.debug(
-    //       "We are approaching retry timeout {}ms, setting backoff to {}ms",
-    //       retryTimeoutInMilliseconds,
-    //       backoffInMilli);
-    // }
-    return 200l;
+    if (retryTimeoutInMilliseconds > 0
+        && (elapsedMilliForTransientIssues + backoffInMilli) > retryTimeoutInMilliseconds) {
+      // If the timeout will be reached before the next backoff, just use the remaining
+      // time (but cannot be negative) - this is the only place when backoff is not in range
+      // min-max.
+      backoffInMilli =
+          Math.max(
+              0,
+              Math.min(
+                  backoffInMilli, retryTimeoutInMilliseconds - elapsedMilliForTransientIssues));
+      logger.debug(
+          "We are approaching retry timeout {}ms, setting backoff to {}ms",
+          retryTimeoutInMilliseconds,
+          backoffInMilli);
+    }
+    return backoffInMilli;
   }
 
   static boolean isNonRetryableHTTPCode(CloseableHttpResponse response, boolean retryHTTP403) {
