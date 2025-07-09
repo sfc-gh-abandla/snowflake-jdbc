ad230dbefde78046c49f1aa562495513fffa0432
Ashrith Bandla <ashrith.bandla@snowflake.com>
2025-07-09 17:12:11 +0000

    made constant backoff 500ms

--- src/main/java/net/snowflake/client/jdbc/RestRequest.java
+++ src/main/java/net/snowflake/client/jdbc/RestRequest.java
@@ -316,7 +316,7 @@ public class RestRequest {
       int retryCount,
       long retryTimeoutInMilliseconds,
       long elapsedMilliForTransientIssues) {
-    long backoffInMilli=200;
+    long backoffInMilli=500;
     // if (isLoginRequest) {
     //   long jitteredBackoffInMilli =
     //       decorrelatedJitterBackoff.getJitterForLogin(previousBackoffInMilli);
